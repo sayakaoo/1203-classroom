736cc22b41e870f39389ffcad69a64e39c279c2f
sayakaoo <nototo2221@gmail.com>
2024-12-04 13:37:24 +0900

    savebutton完成

--- public/script.js
+++ public/script.js
@@ -66,7 +66,7 @@ window.addEventListener('load', function () {
           stop_flg = true;
           break;
         case 'saveButton':
-          $('#save-button').addClass('show');
+          $('.savebutton').addClass('show');
           console.log('ボタン表示!');
           break;
         case 'selectBox':
