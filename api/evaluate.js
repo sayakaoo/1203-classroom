9ddc4f1e24fbfd570946e22de1b4edfa19b9f557
sayakaoo <nototo2221@gmail.com>
2024-12-09 14:52:32 +0900

    変更内容の説明

--- api/evaluate.js
+++ api/evaluate.js
@@ -57,7 +57,8 @@ module.exports = async (req, res) => {
 
   try {
     const response = await openai.chat.completions.create({
-      model: 'gpt-3.5-turbo',
+      model: 'gpt-4o',
+
       messages: [{ role: 'user', content: prompt }],
       max_tokens: 150,
     });
